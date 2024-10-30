ae68e218f5f272ef727a5c96a14ebd346a805260
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-10-30 23:42:01 +0200

    Add "Polsl\Tests\Integration\Api\Json\LoginTest"

---
+++ tests/Integration/Api/Json/LoginTest.php
@@ -0,0 +1,97 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Tests\Integration\Api\Json;
+
+use Tab\Packages\Faker\Faker;
+use Tab\Packages\HttpResponse\ResponseInterface;
+use Tab\Packages\TestCase\Fixtures\Entity\User;
+use Tab\Packages\TestCase\IntegrationTestCase;
+use Tab\Packages\TestCase\Mother\Entity\UserMother;
+use Tab\Tests\TestCase\Application\Client\LoginClient;
+
+/** @internal */
+final class LoginTest extends IntegrationTestCase
+{
+    /**
+     * @dataProvider loginInvalidDataProvider
+     *
+     * @param \Closure(): array{
+     *     user: User,
+     *     password: string,
+     *     expectedError: string,
+     * } $generateParams
+     */
+    public function test_unable_to_login_to_some_accounts(\Closure $generateParams): void
+    {
+        [
+            'user' => $user,
+            'password' => $password,
+            'expectedError' => $expectedError
+        ] = $generateParams();
+        $this->loadEntities($user);
+
+        $jsonSerializer = $this->jsonSerializer();
+        $loginClient = $this->classService(LoginClient::class);
+        $response = $loginClient->login($user->email, $password);
+
+        self::assertSame(ResponseInterface::HTTP_UNAUTHORIZED, $response->statusCode());
+        self::assertJson($response->content());
+
+        /**
+         * @var array{
+         *     error?: string,
+         *     errorMessage?: string,
+         * } $responseData
+         */
+        $responseData = $jsonSerializer->decode($response->content(), true);
+
+        self::assertArrayHasKey('error', $responseData);
+        self::assertArrayHasKey('errorMessage', $responseData);
+        self::assertSame($expectedError, $responseData['error']);
+        self::assertNotEmpty($responseData['errorMessage']);
+    }
+
+    public function test_user_can_login_by_username_and_password(): void
+    {
+        // Arrange
+        $user = UserMother::random();
+        $this->loadEntities($user);
+        $jsonSerializer = $this->jsonSerializer();
+        $loginClient = $this->classService(LoginClient::class);
+
+        // Act
+        $response = $loginClient->login($user->email, $user->password);
+
+        // Assert
+        self::assertSame(ResponseInterface::HTTP_OK, $response->statusCode());
+        self::assertJson($response->content());
+
+        /**
+         * @var array{
+         *     status?: bool,
+         * } $responseData
+         */
+        $responseData = $jsonSerializer->decode($response->content(), true);
+
+        self::assertArrayHasKey('status', $responseData);
+        self::assertTrue($responseData['status']);
+    }
+
+    /** @return iterable<string, array{\Closure}> */
+    public static function loginInvalidDataProvider(): iterable
+    {
+        yield 'invalid password' => [
+            static function (): array {
+                $user = UserMother::random();
+
+                return [
+                    'user' => $user,
+                    'password' => Faker::password(),
+                    'expectedError' => 'Invalid credentials.',
+                ];
+            },
+        ];
+    }
+}
---
+++ tests/TestCase/Application/Client/LoginClient.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Tests\TestCase\Application\Client;
+
+use http\Exception\RuntimeException;
+use Tab\Packages\HttpResponse\ResponseInterface;
+use Tab\Packages\JsonSerializer\JsonSerializerInterface;
+use Tab\Packages\TestCase\Client\KernelBrowserInterface;
+use Tab\Packages\TestCase\Client\RequestInterface;
+
+final class LoginClient implements ClientInterface
+{
+    public function __construct(
+        private readonly KernelBrowserInterface $httpClient,
+        private readonly JsonSerializerInterface $jsonSerializer,
+    ) {
+    }
+
+    public function login(string $userName, string $password): ResponseInterface
+    {
+        $this->httpClient
+            ->followRedirects(false)
+        ;
+        $content = $this->jsonSerializer
+            ->encode(
+                [
+                    'username' => $userName,
+                    'password' => $password,
+                ],
+            )
+        ;
+
+        return $this->httpClient
+            ->request(
+                RequestInterface::METHOD_POST,
+                '/api/login',
+                [],
+                $content,
+                ['CONTENT_TYPE' => 'application/json'],
+            )
+        ;
+    }
+
+    public function withHttpClient(KernelBrowserInterface $httpClient): ClientInterface
+    {
+        throw new RuntimeException('Not implemented.');
+    }
+}
