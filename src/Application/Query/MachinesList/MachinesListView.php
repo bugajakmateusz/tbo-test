89a27745a5785c66564f017455fdb5ec2ce11320
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-02 19:10:50 +0200

    Introduce "Polsl\Application\Query\MachinesList\MachinesListView"

---
+++ src/Application/Query/MachinesList/MachinesListView.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Application\Query\MachinesList;
+
+use Tab\Application\View\MachineView;
+use Tab\Packages\ResourcesList\Fields;
+use Tab\Packages\ResourcesList\TotalItems;
+
+final readonly class MachinesListView
+{
+    /** @var MachineView[] */
+    public array $machines;
+
+    public function __construct(
+        public TotalItems $totalItems,
+        public Fields $fields,
+        MachineView ...$teachers,
+    ) {
+        $this->machines = $teachers;
+    }
+}
