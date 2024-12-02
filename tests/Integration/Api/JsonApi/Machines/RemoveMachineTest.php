b3f76dde09a9fd7c35e3f04788527bd8368e3bd0
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-03 00:08:13 +0200

    Introduce "Polsl\UserInterface\Http\JsonApi\Machines\RemoveMachineAction" action and register path

---
+++ tests/Integration/Api/JsonApi/Machines/RemoveMachineTest.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Tests\Integration\Api\JsonApi\Machines;
+
+use Tab\Application\Schema\MachineSchema;
+use Tab\Packages\Constants\HttpStatusCodes;
+use Tab\Packages\JsonApi\Application\JsonApiKeywords;
+use Tab\Packages\TestCase\Mother\Entity\MachineMother;
+use Tab\Packages\TestCase\Mother\Entity\UserMother;
+use Tab\Tests\TestCase\JsonApiIntegrationTestCase;
+
+/** @internal */
+final class RemoveMachineTest extends JsonApiIntegrationTestCase
+{
+    public function test_logged_user_can_remove_machine(): void
+    {
+        // Arrange
+        $loggedUser = UserMother::random();
+        $machine = MachineMother::random();
+        $this->loadEntities(
+            $loggedUser,
+            $machine,
+        );
+        $client = $this->loggedJsonApiClient(
+            MachineSchema::class,
+            $loggedUser,
+        );
+
+        // Act
+        $response = $client->deleteResource(
+            (string) $machine->id,
+        );
+
+        // Assert
+        self::assertSame(HttpStatusCodes::HTTP_NO_CONTENT, $response->statusCode());
+
+        // Act
+        $jsonApiResponse = $client->requestList(
+            additionalQueryParams: [
+                JsonApiKeywords::FIELDS => [
+                ],
+            ],
+        );
+
+        // Assert
+        $document = $jsonApiResponse->document();
+        self::assertSame(0, $document->resourcesCount());
+    }
+}
