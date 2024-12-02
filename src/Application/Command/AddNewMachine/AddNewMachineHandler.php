14e1cb97def65413c0b98fae57b6c35b07ea4a22
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-02 21:59:12 +0200

    Add "Polsl\Application\Command\AddNewMachine\AddNewMachine" command with handler

---
+++ src/Application/Command/AddNewMachine/AddNewMachineHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Application\Command\AddNewMachine;
+
+use Tab\Domain\Model\Machine\Location;
+use Tab\Domain\Model\Machine\Machine;
+use Tab\Domain\Model\Machine\MachineRepositoryInterface;
+
+final readonly class AddNewMachineHandler
+{
+    public function __construct(
+        private MachineRepositoryInterface $machineRepository,
+    ) {
+    }
+
+    public function __invoke(AddNewMachine $command): void
+    {
+        $machine = Machine::create(
+            Location::fromString($command->location),
+            $command->positionsNumber,
+            $command->positionsCapacity,
+        );
+
+        $this->machineRepository
+            ->add($machine)
+        ;
+    }
+}
