8f35173d39d71dfa76244e9c5e4b19e5fe8084d4
kmekarski <72306134+kmekarski@users.noreply.github.com>
2024-12-12 02:30:20 +0200

    added templates for all forms

--- landings/frontend/src/app/users/components/add-user-page/add-user-page.component.ts
+++ landings/frontend/src/app/users/components/add-user-page/add-user-page.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-add-user-page',
   templateUrl: './add-user-page.component.html',
-  styleUrls: ['./add-user-page.component.scss']
+  styleUrls: ['./add-user-page.component.scss'],
 })
 export class AddUserPageComponent {
-
+  userRoleOptions = [
+    {
+      name: 'Admin',
+      value: 'admin',
+    },
+    {
+      name: 'Pracownik biurowy',
+      value: 'office',
+    },
+    {
+      name: 'Pracownik magazynu',
+      value: 'warehouse',
+    },
+    {
+      name: 'Kurier',
+      value: 'courier',
+    },
+  ];
 }
