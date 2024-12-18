da09a02cc6dbf2d9fc64aa7aaa3698119cb57c45
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-19 01:55:16 +0200

    Introduce "Polsl\UserInterface\Http\JsonApi\Snacks\CreateNewSnackAction" action and register path to it

---
+++ src/UserInterface/Http/JsonApi/Snacks/CreateNewSnackAction.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\UserInterface\Http\JsonApi\Snacks;
+
+use Symfony\Component\HttpFoundation\Request;
+use Tab\Application\Command\AddNewSnack\AddNewSnack;
+use Tab\Application\Schema\SnackSchema;
+use Tab\Application\Service\ApiProblemJsonResponseFactory;
+use Tab\Packages\Constants\HttpStatusCodes;
+use Tab\Packages\JsonApi\Application\Resource;
+use Tab\Packages\JsonApi\Contracts\JsonApiSerializerInterface;
+use Tab\Packages\JsonApi\ResponseFactory\JsonApiResponseFactoryInterface;
+use Tab\Packages\MessageBus\Contracts\CommandBusInterface;
+use Tab\Packages\Responder\Response\ResponseInterface;
+use Tab\Packages\Validator\ValidatorInterface;
+
+final readonly class CreateNewSnackAction
+{
+    public function __construct(
+        private JsonApiSerializerInterface $jsonApiSerializer,
+        private CommandBusInterface $commandBus,
+        private JsonApiResponseFactoryInterface $jsonApiResponseFactory,
+        private ValidatorInterface $validator,
+        private ApiProblemJsonResponseFactory $apiProblemJsonResponseFactory,
+    ) {
+    }
+
+    public function __invoke(
+        Request $request,
+    ): ResponseInterface {
+        $resource = $this->jsonApiSerializer
+            ->decodeResource(
+                (string) $request->getContent(),
+            )
+        ;
+        $createNewSnackCommand = $this->createCommand(
+            $resource,
+        );
+
+        $violations = $this->validator
+            ->validate(
+                $createNewSnackCommand,
+            )
+        ;
+
+        if (!$violations->isEmpty()) {
+            return $this->apiProblemJsonResponseFactory
+                ->unprocessableEntity(
+                    $violations,
+                )
+            ;
+        }
+
+        $this->commandBus->handle(
+            $createNewSnackCommand,
+        );
+
+        return $this->jsonApiResponseFactory
+            ->resourceResponse(
+                statusCode: HttpStatusCodes::HTTP_CREATED,
+            )
+        ;
+    }
+
+    private function createCommand(
+        Resource $resource,
+    ): AddNewSnack {
+        $resource->checkExpectedType(
+            SnackSchema::TYPE,
+        );
+        /**
+         * @var array{
+         *     name?: string,
+         * } $attributes
+         */
+        $attributes = $resource->attributes();
+
+        return new AddNewSnack(
+            $attributes[SnackSchema::ATTRIBUTE_NAME] ?? '',
+        );
+    }
+}
