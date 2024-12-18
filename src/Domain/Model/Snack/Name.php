6eb76bba7c2601793814cbfb377bc236dd6216a2
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-19 01:48:03 +0200

    Create "Polsl\Domain\Model\Snack\Name" VO

---
+++ src/Domain/Model/Snack/Name.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Domain\Model\Snack;
+
+use Tab\Domain\SanitizedString;
+
+final class Name
+{
+    public const MAX_LENGTH = 255;
+
+    private function __construct(
+        private readonly string $text,
+    ) {
+    }
+
+    public static function fromString(
+        string $text,
+    ): self {
+        $safeText = SanitizedString::create($text);
+        $safeText->checkIsEmpty('Name');
+        $safeText->checkMaxLength(
+            self::MAX_LENGTH,
+            'Name',
+        );
+
+        return new self(
+            $safeText->toString(),
+        );
+    }
+
+    public function toString(): string
+    {
+        return $this->text;
+    }
+}
---
+++ tests/Unit/Domain/Model/Snack/NameTest.php
@@ -0,0 +1,143 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Tests\Unit\Domain\Model\Snack;
+
+use Tab\Domain\DomainException;
+use Tab\Domain\Model\Snack\Name;
+use Tab\Packages\Faker\Faker;
+use Tab\Packages\TestCase\UnitTestCase;
+
+/** @internal */
+final class NameTest extends UnitTestCase
+{
+    /**
+     * @param \Closure(): array{
+     *      nameString: string,
+     *      expectedExceptionMessage: string,
+     *  } $createParams
+     *
+     * @dataProvider incorrectNameProvider
+     */
+    public function test_incorrect_name_throws_exception(
+        \Closure $createParams,
+    ): void {
+        // Arrange
+        [
+            'nameString' => $nameString,
+            'expectedExceptionMessage' => $expectedExceptionMessage,
+        ] = $createParams();
+
+        // Expect
+        $this->expectException(
+            DomainException::class,
+        );
+        $this->expectExceptionMessage(
+            $expectedExceptionMessage,
+        );
+
+        // Act
+        Name::fromString(
+            $nameString,
+        );
+    }
+
+    /**
+     * @param \Closure(): array{
+     *     nameString: string,
+     *     expectedName: string,
+     * } $createParams
+     *
+     * @dataProvider correctNameProvider
+     */
+    public function test_correct_name_is_accepted(
+        \Closure $createParams,
+    ): void {
+        // Arrange
+        [
+            'nameString' => $nameString,
+            'expectedName' => $expectedName,
+        ] = $createParams();
+
+        // Expect
+        $Name = Name::fromString(
+            $nameString,
+        );
+
+        self::assertSame(
+            $expectedName,
+            $Name->toString(),
+        );
+    }
+
+    /** @return iterable<string,array{\Closure}> */
+    public static function incorrectNameProvider(): iterable
+    {
+        yield 'empty' => [
+            static fn (): array => [
+                'nameString' => '',
+                'expectedExceptionMessage' => 'Name cannot be empty.',
+            ],
+        ];
+        yield 'empty html' => [
+            static fn (): array => [
+                'nameString' => '<p></p>',
+                'expectedExceptionMessage' => 'Name cannot be empty.',
+            ],
+        ];
+        yield 'empty space' => [
+            static fn (): array => [
+                'nameString' => ' ',
+                'expectedExceptionMessage' => 'Name cannot be empty.',
+            ],
+        ];
+        yield 'empty tab' => [
+            static fn (): array => [
+                'nameString' => "\t",
+                'expectedExceptionMessage' => 'Name cannot be empty.',
+            ],
+        ];
+        yield 'empty newline' => [
+            static fn (): array => [
+                'nameString' => "\r\n",
+                'expectedExceptionMessage' => 'Name cannot be empty.',
+            ],
+        ];
+        yield 'too long' => [
+            static fn (): array => [
+                'nameString' => Faker::hexBytes(
+                    256,
+                ),
+                'expectedExceptionMessage' => "Name length cannot be higher than '255', '256' passed.",
+            ],
+        ];
+    }
+
+    /** @return iterable<string,array{\Closure}> */
+    public static function correctNameProvider(): iterable
+    {
+        yield 'name' => [
+            static function (): array {
+                $name = Faker::text();
+
+                return [
+                    'nameString' => $name,
+                    'expectedName' => $name,
+                ];
+            },
+        ];
+        yield 'html name' => [
+            static fn (): array => [
+                'nameString' => '<p>Test</p>',
+                'expectedName' => 'Test',
+            ],
+        ];
+        yield 'whitespaces name' => [
+            static fn (): array => [
+                'nameString' => " Test\t",
+                'expectedName' => 'Test',
+            ],
+        ];
+    }
+}
