1c725541a5324cce4065efecad6694f2d9b364ff
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-19 01:52:09 +0200

    Add "Polsl\Application\Command\AddNewSnack\AddNewSnack" command with handler

---
+++ src/Application/Command/AddNewSnack/AddNewSnackHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Application\Command\AddNewSnack;
+
+use Tab\Domain\Model\Snack\Name;
+use Tab\Domain\Model\Snack\Snack;
+use Tab\Domain\Model\Snack\SnackRepositoryInterface;
+
+final readonly class AddNewSnackHandler
+{
+    public function __construct(
+        private SnackRepositoryInterface $snackRepository,
+    ) {
+    }
+
+    public function __invoke(
+        AddNewSnack $command,
+    ): void {
+        $name = Name::fromString(
+            $command->name,
+        );
+        $snack = Snack::create($name);
+
+        $this->snackRepository
+            ->add($snack)
+        ;
+    }
+}
