9a434298976243355b9837cac81839ab88a94e63
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-20 01:04:37 +0200

    Introduce "Polsl\Application\Schema\MachineSnackSchema" schema

--- src/Infrastructure/Neomerx/NeomerxLazySchemaContainer.php
+++ src/Infrastructure/Neomerx/NeomerxLazySchemaContainer.php
@@ -9,8 +9,10 @@ use Neomerx\JsonApi\Contracts\Schema\SchemaInterface;
 use Neomerx\JsonApi\Factories\Factory;
 use Psr\Container\ContainerInterface;
 use Tab\Application\Schema\MachineSchema;
+use Tab\Application\Schema\MachineSnackSchema;
 use Tab\Application\Schema\SnackSchema;
 use Tab\Application\Schema\UserSchema;
+use Tab\Application\View\MachineSnackView;
 use Tab\Application\View\MachineView;
 use Tab\Application\View\SnackView;
 use Tab\Application\View\UserView;
@@ -23,6 +25,7 @@ final class NeomerxLazySchemaContainer implements SchemaContainerInterface
         MachineView::class => MachineSchema::class,
         SnackView::class => SnackSchema::class,
         UserView::class => UserSchema::class,
+        MachineSnackView::class => MachineSnackSchema::class,
     ];
 
     /** @var array<class-string,SchemaInterface> */
