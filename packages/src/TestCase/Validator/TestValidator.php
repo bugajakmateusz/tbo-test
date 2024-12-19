98231b4b8d03bd161db449015d5d8200489ac7d5
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-19 21:40:13 +0200

    Add "Validator" to TestCase package

---
+++ packages/src/TestCase/Validator/TestValidator.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Packages\TestCase\Validator;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\GroupSequence;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface as SymfonyValidatorInterface;
+use Tab\Packages\Validator\SymfonyViolation;
+use Tab\Packages\Validator\ViolationInterface;
+use Tab\Packages\Validator\Violations;
+
+final readonly class TestValidator
+{
+    public function __construct(private SymfonyValidatorInterface $validator)
+    {
+    }
+
+    /**
+     * @param null|Constraint|Constraint[]     $constraints
+     * @param null|array<GroupSequence|string> $validationGroups
+     */
+    public function validate(
+        mixed $data,
+        Constraint|array $constraints = null,
+        ?array $validationGroups = null,
+    ): Violations {
+        $violations = $this->validator
+            ->validate(
+                $data,
+                $constraints,
+                $validationGroups,
+            )
+        ;
+
+        return Violations::fromViolations(
+            ...\array_map(
+                static function (ConstraintViolationInterface $constraintViolation): ViolationInterface {
+                    return new SymfonyViolation($constraintViolation);
+                },
+                \iterator_to_array($violations),
+            ),
+        );
+    }
+}
---
+++ packages/src/TestCase/Validator/ViolationsTest.php
@@ -0,0 +1,86 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Packages\TestCase\Validator;
+
+use Tab\Packages\TestCase\UnitTestCase;
+use Tab\Packages\Validator\ViolationInterface;
+use Tab\Packages\Validator\Violations;
+
+/** @internal */
+final class ViolationsTest extends UnitTestCase
+{
+    /**
+     * @param \Closure(): array{
+     *     violations: Violations,
+     *     expectedIsEmpty: bool,
+     * } $paramsGenerator
+     *
+     * @dataProvider emptyViolationsProvider
+     */
+    public function test_empty_violations_list(\Closure $paramsGenerator): void
+    {
+        // Arrange
+        [
+            'violations' => $violations,
+            'expectedIsEmpty' => $expectedIsEmpty,
+        ] = $paramsGenerator();
+
+        // Act
+        $isEmpty = $violations->isEmpty();
+
+        // Assert
+        self::assertSame($isEmpty, $expectedIsEmpty);
+    }
+
+    public function test_violations_list_can_be_converted_to_array(): void
+    {
+        // Arrange
+        $violation = self::createMockViolation();
+        $violations = Violations::fromViolations($violation);
+
+        // Act
+        $violationsArray = $violations->toArray();
+
+        // Assert
+        self::assertSame([$violation], $violationsArray);
+    }
+
+    /** @return iterable<string,array{\Closure}> */
+    public static function emptyViolationsProvider(): iterable
+    {
+        yield 'empty' => [
+            static fn (): array => [
+                'violations' => Violations::fromViolations(),
+                'expectedIsEmpty' => true,
+            ],
+        ];
+
+        yield 'not empty' => [
+            static function (): array {
+                $violation = self::createMockViolation();
+
+                return [
+                    'violations' => Violations::fromViolations($violation),
+                    'expectedIsEmpty' => false,
+                ];
+            },
+        ];
+    }
+
+    private static function createMockViolation(): ViolationInterface
+    {
+        return new class() implements ViolationInterface {
+            public function propertyPath(): string
+            {
+                throw new \RuntimeException('Not implemented.');
+            }
+
+            public function message(): string
+            {
+                throw new \RuntimeException('Not implemented.');
+            }
+        };
+    }
+}
