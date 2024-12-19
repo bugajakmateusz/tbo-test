89672517a5df5fca9f9904686fc761363a27c9c8
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-19 21:37:04 +0200

    Introduce "Polsl\Domain\Model\User\Password" Value Object

---
+++ src/Domain/Model/User/Password.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Domain\Model\User;
+
+use Tab\Domain\DomainException;
+use Tab\Packages\PasswordHasher\PasswordHasherInterface;
+
+final class Password
+{
+    public const MIN_LENGTH = 8;
+    public const MAX_LENGTH = 64;
+
+    private function __construct(private readonly string $password)
+    {
+    }
+
+    public static function hash(string $password, PasswordHasherInterface $passwordHasher): self
+    {
+        $length = \mb_strlen($password);
+        if ($length < self::MIN_LENGTH || $length > self::MAX_LENGTH) {
+            $minLength = self::MIN_LENGTH;
+            $maxLength = self::MAX_LENGTH;
+
+            throw new DomainException(
+                "Password length must be between {$minLength} and {$maxLength} characters. Given password has {$length} characters.",
+            );
+        }
+
+        return new self(
+            $passwordHasher->hash($password),
+        );
+    }
+
+    public function toString(): string
+    {
+        return $this->password;
+    }
+}
