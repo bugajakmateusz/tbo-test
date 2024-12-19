9539c3902dc01610eab5406d30d92f75d07bdb4e
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-19 19:26:28 +0200

    Quantity added to SnackSchema and SnackView

--- src/Application/View/SnackView.php
+++ src/Application/View/SnackView.php
@@ -8,16 +8,19 @@ namespace Tab\Application\View;
  * @phpstan-type SnackData array{
  *      id?: int,
  *      name?: string,
+ *      quantity?: int,
  *  }
  */
 final readonly class SnackView
 {
     public const FIELD_RAW_ID = 'id';
     public const FIELD_RAW_NAME = 'name';
+    public const FIELD_RAW_QUANTITY = 'quantity';
 
     private function __construct(
         public int $id,
         public string $name,
+        public int $quantity,
     ) {}
 
     /** @param SnackData $data */
@@ -27,6 +30,7 @@ final readonly class SnackView
         return new self(
             $data[self::FIELD_RAW_ID] ?? 0,
             $data[self::FIELD_RAW_NAME] ?? '',
+            $data[self::FIELD_RAW_QUANTITY] ?? 0,
         );
     }
 }
