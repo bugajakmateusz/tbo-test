c3291b1b39444932d8014458aedb6a926067dc32
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-19 02:01:11 +0200

    Add "Polsl\Domain\Model\Snack\SnackRepositoryInterface::get" method

--- src/Domain/Model/Snack/SnackRepositoryInterface.php
+++ src/Domain/Model/Snack/SnackRepositoryInterface.php
@@ -6,6 +6,10 @@ namespace Tab\Domain\Model\Snack;
 
 interface SnackRepositoryInterface
 {
+    public function get(
+        int $snackId,
+    ): Snack;
+
     public function add(
         Snack $snack,
     ): void;
