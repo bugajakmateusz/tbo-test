1a02bc8057e4b44ce0f0b08b48265e4b07fbebe0
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-20 23:38:46 +0200

    Introduce "Polsl\Domain\Model\Machine\SnackPosition" VO

---
+++ src/Domain/Model/Machine/SnackPosition.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Domain\Model\Machine;
+
+use Tab\Domain\SanitizedString;
+
+final class SnackPosition
+{
+    public const MAX_LENGTH = 3;
+
+    private function __construct(private readonly string $text)
+    {
+    }
+
+    public static function fromString(string $text): self
+    {
+        $safeText = SanitizedString::create($text);
+        $safeText->checkIsEmpty('Position');
+        $safeText->checkMaxLength(self::MAX_LENGTH, 'Position');
+
+        return new self($safeText->toString());
+    }
+
+    public function toString(): string
+    {
+        return $this->text;
+    }
+}
