9aac4320bfeb0cef7cc4952e78493d0a6294fe9a
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-21 11:11:13 +0200

    Introduce "Polsl\Application\Command\AddNewMachineSnack\AddNewMachineSnack" command with handler

---
+++ src/Application/Command/AddNewMachineSnack/AddNewMachineSnack.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Application\Command\AddNewMachineSnack;
+
+final readonly class AddNewMachineSnack
+{
+    public function __construct(
+        public int $machineId,
+        public int $snackId,
+        public int $quantity,
+        public string $position,
+    ) {
+    }
+}
