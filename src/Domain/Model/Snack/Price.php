cf38616800613991586ed1d48c6a0c1eff136328
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-23 02:24:08 +0200

    Introduce "Polsl\Domain\Model\Snack\Price" class with Doctrine mapping

---
+++ src/Domain/Model/Snack/Price.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tab\Domain\Model\Snack;
+
+use Tab\Domain\DomainException;
+use Tab\Domain\Model\Machine\Machine;
+use Tab\Domain\Service\ClockInterface;
+
+class Price
+{
+    private int $id;
+
+    private function __construct(
+        private int $machineId,
+        private int $snackId,
+        private float $price,
+        private \DateTimeImmutable $createdAt,
+    ) {}
+
+    public static function create(
+        Machine $machine,
+        Snack $snack,
+        float $price,
+        ClockInterface $clock,
+    ): self {
+        if ($price <= 0) {
+            throw new DomainException('Price cannot be lower than or equal 0.');
+        }
+
+        return new self(
+            $machine->id(),
+            $snack->id(),
+            $price,
+            $clock->now(),
+        );
+    }
+}
--- tests/TestCase/Application/Mother/MachineMother.php
+++ tests/TestCase/Application/Mother/MachineMother.php
@@ -7,6 +7,7 @@ namespace Tab\Tests\TestCase\Application\Mother;
 use Tab\Domain\Model\Machine\Location;
 use Tab\Domain\Model\Machine\Machine;
 use Tab\Packages\Faker\Faker;
+use Tab\Tests\TestCase\Application\PropertyAccess\PropertyManipulator;
 
 final class MachineMother
 {
@@ -20,10 +21,19 @@ final class MachineMother
         ?int $positionNumber = null,
         ?int $positionCapacity = null,
     ): Machine {
-        return Machine::create(
+        $propertyManipulator = PropertyManipulator::getInstance();
+        $machine = Machine::create(
             Location::fromString($location ?? Faker::text()),
             $positionNumber ?? Faker::int(),
             $positionCapacity ?? Faker::int(),
         );
+
+        $propertyManipulator->propertySet(
+            $machine,
+            'id',
+            Faker::intId(),
+        );
+
+        return $machine;
     }
 }
