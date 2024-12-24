9614003500bb6036a6742c950f3b76a417216c0e
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2024-12-24 17:03:09 +0200

    Warehouse Prices History table introduced

---
+++ db/migrations/20230914115228_create_warehouse_prices_history_table.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+use Tab\Packages\Constants\Database\Tables;
+
+final class CreateWarehousePricesHistoryTable extends AbstractMigration
+{
+    public function change(): void
+    {
+        $table = $this->table(
+            Tables::WAREHOUSE_PRICES_HISTORY,
+            [
+                'id' => Tables\WarehousePricesHistory::FIELD_BUY_ID,
+            ],
+        );
+        $table
+            ->addColumn(
+                Tables\WarehousePricesHistory::FIELD_SNACK_ID,
+                'integer',
+                [
+                    'null' => false,
+                ],
+            )
+            ->addColumn(
+                Tables\WarehousePricesHistory::FIELD_BUY_PRICE,
+                'float',
+                [
+                    'null' => false,
+                    'signed' => false,
+                ],
+            )
+            ->addColumn(
+                Tables\WarehousePricesHistory::FIELD_BUY_DATE,
+                'datetime',
+                [
+                    'null' => false,
+                ],
+            )
+            ->addForeignKey(
+                Tables\WarehousePricesHistory::FIELD_SNACK_ID,
+                Tables::SNACKS,
+                Tables\Snacks::FIELD_SNACK_ID,
+                [
+                    'delete' => 'NO_ACTION',
+                    'update' => 'NO_ACTION',
+                ],
+            );
+        $table->create();
+    }
+}
