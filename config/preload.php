499076612c0e52fed079a21dda800c4c5c73eefb
Mateusz Bugajak <mateusz.bugajak@zwolnienizteorii.pl>
2025-01-07 18:46:50 +0100

    CSP and CORS

--- config/preload.php
+++ config/preload.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-$path = \dirname(__DIR__) . '/var/cache/prod/Tab_KernelProdContainer.preload.php';
+$path = \dirname(__DIR__) . '/var/cache/prod/Polsl_KernelProdContainer.preload.php';
 if (\file_exists($path)) {
     // Temporary increase memory limit
     \ini_set('memory_limit', '160M');
